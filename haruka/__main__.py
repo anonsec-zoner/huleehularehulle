03f3edfd983bd4d34290ca62553ad65faf2056ad
shivamkchoudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-06-22 20:24:02 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -153,14 +153,11 @@ def send_start(bot, update):
 Earth. That is my price.!
 """
 
-
    
     keyboard = [[InlineKeyboardButton(text="🛠 Control panel", callback_data="cntrl_panel_M")]]
     keyboard += [[InlineKeyboardButton(text="🇺🇸 Language", callback_data="set_lang_"), 
         InlineKeyboardButton(text="❔ Help", callback_data="help_back")]]
-
-    update.effective_message.reply_text(text, reply_markup=InlineKeyboardMarkup(keyboard), parse_mode=ParseMode.MARKDOWN)
-
+update.effective_message.reply_text(PM_START.format(escape_markdown(first_name), bot.first_name), reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
 
 
 def control_panel(bot, update):
