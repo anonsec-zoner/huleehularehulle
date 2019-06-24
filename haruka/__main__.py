cd96419f8f462251164cbc295249ea595c62e4fe
shivamkchoudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2019-06-24 20:52:18 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -546,7 +546,6 @@ def main():
     settings_handler = CommandHandler("settings", get_settings)
     settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    donate_handler = CommandHandler("donate", donate)
     migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
 
     # dispatcher.add_handler(test_handler)
@@ -556,7 +555,6 @@ def main():
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
     dispatcher.add_handler(migrate_handler)
-    dispatcher.add_handler(donate_handler)
 
     # dispatcher.add_error_handler(error_callback)
 
